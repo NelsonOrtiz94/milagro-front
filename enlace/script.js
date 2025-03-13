96cd388c559e42aef3d8785132e6814db0dce9c9
NelsonOrtiz94 <ortnn990@gmail.com>
2025-03-13 10:57:38 -0500

    Primer comentario de landing page

---
+++ enlace/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+      loop: true,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  });
+  
