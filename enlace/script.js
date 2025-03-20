f13f1bd0ceb19b6ee249ed279099ed55e884f646
NelsonOrtiz94 <ortnn990@gmail.com>
2025-03-20 10:53:24 -0500

    Se adecua una interfaz visual más amena para la UI

--- enlace/script.js
+++ enlace/script.js
@@ -1,13 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const images = document.querySelectorAll(".testimonio img");
-
-  // Lista de imágenes de respaldo en orden (hombre, mujer, hombre, mujer)
-  const fallbackImages = ["img/default-male.jpg", "img/default-female.jpg", "img/default-male.jpg", "img/default-female.jpg"];
-
-  images.forEach((img, index) => {
-      img.onerror = function () {
-          this.onerror = null; // Evita el bucle infinito
-          this.src = fallbackImages[index]; // Asigna la imagen correspondiente
-      };
+window.addEventListener("wheel", function(event) {
+  event.preventDefault();
+  window.scrollBy({
+      top: event.deltaY * 0.5, // Ajusta este valor para cambiar la sensibilidad
+      behavior: "smooth"
   });
-});
+}, { passive: false });
+
