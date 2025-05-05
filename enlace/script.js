7ff448c56a91bc91c86d97655c0bba6e4ffd220b
NelsonOrtiz94 <ortnn990@gmail.com>
2025-05-05 11:23:33 -0500

    Implementaciones para Google ads

--- enlace/script.js
+++ enlace/script.js
@@ -1,8 +1,19 @@
-window.addEventListener("wheel", function(event) {
-  event.preventDefault();
-  window.scrollBy({
-      top: event.deltaY * 0.5, // Ajusta este valor para cambiar la sensibilidad
-      behavior: "smooth"
-  });
-}, { passive: false });
+window.addEventListener(
+  "wheel",
+  function (event) {
+    event.preventDefault();
+    window.scrollBy({
+      top: event.deltaY * 10, // Ajusta este valor para cambiar la sensibilidad
+      behavior: "smooth",
+    });
+  },
+  { passive: false }
+);
 
+document.querySelectorAll('a[href*="w.app/"]').forEach((boton) => {
+  boton.addEventListener("click", () => {
+    gtag("event", "conversion", {
+      send_to: "AW-16932118716/rFSvCLLyqa0aELzB7ok_",
+    });
+  });
+});
