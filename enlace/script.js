f0777f221b0f4a4069fd92c97643d4229b2b1dbe
NelsonOrtiz94 <ortnn990@gmail.com>
2025-05-05 14:18:31 -0500

    Cambios en rutas y en scroll para analizar comportamiento en host

--- enlace/script.js
+++ enlace/script.js
@@ -1,15 +1,3 @@
-window.addEventListener(
-  "wheel",
-  function (event) {
-    event.preventDefault();
-    window.scrollBy({
-      top: event.deltaY * 10, // Ajusta este valor para cambiar la sensibilidad
-      behavior: "smooth",
-    });
-  },
-  { passive: false }
-);
-
 document.querySelectorAll('a[href*="w.app/"]').forEach((boton) => {
   boton.addEventListener("click", () => {
     gtag("event", "conversion", {
