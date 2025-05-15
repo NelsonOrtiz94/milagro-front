c2ee7c032c9d4289a6df4401a865a42666ab3023
NelsonOrtiz94 <ortnn990@gmail.com>
2025-05-15 09:14:30 -0500

    Más cambios para poder dar solucion a conflictos

--- enlace/script.js
+++ enlace/script.js
@@ -1,9 +1,1 @@
-// Ejecutar el evento solo cuando se haga clic o envíe un formulario
-document.querySelectorAll('.whatsapp-float, .call-float').forEach(boton => {
-  boton.addEventListener('click', () => {
-    gtag('event', 'conversion', {
-      'send_to': 'AW-16932118716/rFSvCLLyqa0aELzB7ok_'
-    });
-  });
-});
 
